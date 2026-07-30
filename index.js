f23b8144ede7e23f03925c50b3f2a2b9a1b9b0fc
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-07-30 20:58:32 +0600

    chore(db): initialize MongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
